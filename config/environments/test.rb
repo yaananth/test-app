09e8ce43807ce545e1b59537a97d5db546b65a87
Yashwanth Anantharaju <yaananth@users.noreply.github.com>
2020-07-30 12:26:54 -0400

    Update test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,6 +13,12 @@ Rails.application.configure do
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
+  
+  # Enable stdout logger
+  config.logger = Logger.new(STDOUT)
+
+  # Set log level
+  config.log_level = :INFO
 
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
